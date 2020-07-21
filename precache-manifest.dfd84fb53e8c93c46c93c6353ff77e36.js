eebf71f9771c31ef5581fc90c7cbdd9b0cc7f4ef
jiwon_lala <s26788761@naver.com>
2020-07-21 21:10:06 +0900

    Updates

---
+++ precache-manifest.dfd84fb53e8c93c46c93c6353ff77e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bed7368e3608ffd09f145f555ba68e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f2f5c47e063e2c19e2b1",
+    "url": "/movie_app_2020/static/css/main.7a160fdc.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f5c47e063e2c19e2b1",
+    "url": "/movie_app_2020/static/js/main.b03542cd.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
